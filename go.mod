e11ba9f4edf299e88b86974c3b2cd6929438a8df
Tamal Saha <tamal@appscode.com>
2021-04-25 04:25:34 -0700

    added-all

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tamalsaha/kstatus-demo
 
 go 1.16
+
+require sigs.k8s.io/kustomize/kstatus v0.0.2
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"sigs.k8s.io/kustomize/kstatus/status"
+)
 
+func main() {
+	status.
 }
