865411a94ff175a47e827e0000c26d3f1626d80e
Tamal Saha <tamal@appscode.com>
2021-04-25 05:37:48 -0700

    added-all

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tamalsaha/kstatus-demo
 
 go 1.16
 
-require sigs.k8s.io/kustomize/kstatus v0.0.2
+require (
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
+	sigs.k8s.io/cli-utils v0.25.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"sigs.k8s.io/kustomize/kstatus/status"
+	"context"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"path/filepath"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 )
 
 func main() {
-	status.
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	dc := dynamic.NewForConfigOrDie(config)
+	nodes, err := dc.Resource(schema.GroupVersionResource{
+		Group:    "",
+		Version:  "v1",
+		Resource: "nodes",
+	}).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, obj := range nodes.Items {
+		s, err := status.Compute(&obj)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v", *s)
+	}
 }
